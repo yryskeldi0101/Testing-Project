4d50881085d1c0ff517424d4245569764f0b6e3d
yryskeldi0101 <122298326+yryskeldi0101@users.noreply.github.com>
2023-07-31 23:20:39 +0600

    create user cards

---
+++ src/utils/constants/constants.ts
@@ -0,0 +1,47 @@
+export interface MyData {
+  img: string;
+  id: number;
+}
+
+export const userImgArray: MyData[] = [
+  {
+    img: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2F1.bp.blogspot.com%2F--ibXChs4NtU%2FXFLo6nppSWI%2FAAAAAAAAVRY%2FbTCuOIQ7h5ownjY4nxZZgpV2eLTk3HCHwCLcBGAs%2Fs1600%2FRobert%252BDowney%252BJr.%252Bgo%252Bprofiel%252B1.jpg&f=1&nofb=1&ipt=5b432a601825f6553c67c97ae01b652506686bdac1bd9d5dac15ae0447161781&ipo=images',
+    id: 1
+  },
+  {
+    img: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2F98%2F71%2Fc1%2F9871c11d3197897e85d20ac51e35a972.png&f=1&nofb=1&ipt=07aba1113901e5bb7792a306703e83d629e70cb258479b00500d065165108206&ipo=images',
+    id: 2
+  },
+  {
+    img: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fst1.bollywoodlife.com%2Fwp-content%2Fuploads%2F2019%2F04%2Fchris-2.jpg&f=1&nofb=1&ipt=04f8e857183a039b3fdd4e4aac8cdb67abc6e5e68b894a94003c13a7ccfdfe52&ipo=images',
+    id: 3
+  },
+  {
+    img: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fen%2Ff%2Ff6%2FScarlett_Johansson_as_Black_Widow.jpg&f=1&nofb=1&ipt=2eddbf3f3d6e9f741d189fb80e4c34c0c5cb14e632f0aa0d9041e8b8f29a39c4&ipo=images',
+    id: 4
+  },
+  {
+    img: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.ecranlarge.com%2Fuploads%2Fimage%2F000%2F974%2Fdwayne-johnson-black-adam-974657.jpeg&f=1&nofb=1&ipt=110b90ca336aee4f61622291e5a004282d945ee2ab23bba3b10e890a6d504504&ipo=images',
+    id: 5
+  },
+  {
+    img: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2F9a%2F32%2F7e%2F9a327eaf02aad14c2aabce7f0a3e2a7c.jpg&f=1&nofb=1&ipt=df4d1b427efc03cd8a75a2d84f2386e5674c8d711eb67b652ed92bc7d8c2f69f&ipo=images',
+    id: 6
+  },
+  {
+    img: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2Fd9%2F91%2Fc2%2Fd991c2628b7a5a36529b0d16b7b82ce0.jpg&f=1&nofb=1&ipt=9269fe865228fd6f4c3f6ef5a08b40118a03afdc1dc5abb65728571fb5f202dd&ipo=images',
+    id: 7
+  },
+  {
+    img: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2F736x%2Feb%2F0e%2F60%2Feb0e60997dd3258bd1f2478aba5b7ac4.jpg&f=1&nofb=1&ipt=03dba06ed9b693114396b70361eb0fd07d0b858b2db207cf718066e02d9f3533&ipo=images',
+    id: 8
+  },
+  {
+    img: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2F736x%2F2d%2Fd3%2Fd2%2F2dd3d206693c04cbd312a4b10f6ac4c0.jpg&f=1&nofb=1&ipt=da18c7464c44362a46e18e000e45815553934e9459e14556afdff8123a708bc0&ipo=images',
+    id: 9
+  },
+  {
+    img: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2F08%2F8a%2F70%2F088a70873c96219231dec37f8d484ae2.jpg&f=1&nofb=1&ipt=fa1b9d29d690645f524675278ad111bd68e8cffa52e46127894b2299b0253e9f&ipo=images',
+    id: 10
+  }
+];
